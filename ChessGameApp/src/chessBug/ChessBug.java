318e7562960a945605bc79b3b83354f929b3a6a9
aleaphia <shosh.b.lily@gmail.com>
2025-01-26 16:42:08 -0600

    Started laying out GUI frame

---
+++ ChessGameApp/src/chessBug/ChessBug.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chessBug;
+import listHelper.SavableList;
+import chessGame.*;
+import java.io.*;
+import java.util.*;
+
+import javafx.application.*;
+import javafx.stage.Stage;
+import javafx.geometry.*;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+/**
+ *
+ * @author shosh
+ */
+public class ChessBug extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    //Create stage layout ======================================================
+        //Main pane
+        VBox mainPane = new VBox();
+        mainPane.getStyleClass().add("background");
+        
+        //Create Menu
+        MenuBar menuBar = new MenuBar();
+        mainPane.getChildren().add(menuBar);
+        
+        //Create menuBar options
+        String[] menus = {"Home", "Games" , "Settings"};
+        String[][] menuOptions = {
+            {"DashBoard"}, // Home
+            {}, // Games
+            {}  // Setting
+        };
+        fillMenuBar(menuBar, menus, menuOptions);
+        
+
+        //Scene and Stage ------------------------------------------------------
+        primaryStage.setTitle("ChessBug"); //Name for application stage
+        Scene mainScene = new Scene(mainPane, 800, 600); //Add mainPane to the mainScene
+        primaryStage.setScene(mainScene);//Add mainScene to primaryStage
+        
+        //Style
+        mainScene.getStylesheets().add("Styles.css");
+       
+        //Display
+        primaryStage.show();
+        //-----------------------------------------------------------------------
+
+        //======================================================================
+    }
+    
+    private void fillMenuBar(MenuBar menuBar, String[] menus, String[][] menuOptions){
+        //Add each menu to the MenuBar
+        for (int i = 0; i < menus.length; i++){
+            Menu menu = new Menu(menus[i]); //Create menu
+            menuBar.getMenus().add(menu); //Add to container
+            
+            //Add each menu option to the menu
+            for (int j = 0; j < menuOptions[i].length; j++){
+                MenuItem menuItem = new MenuItem(menuOptions[i][j]); //Create menuItem
+                menu.getItems().add(menuItem); //Add to container
+            }
+            
+            
+        }
+    }
+    
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
