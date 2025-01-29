ae712accab57e82c37d57dab72429eb267d4e556
aleaphia <shosh.b.lily@gmail.com>
2025-01-29 17:03:43 -0600

    Modified switch syntax

--- ChessGameApp/src/chessBug/ChessBug.java
+++ ChessGameApp/src/chessBug/ChessBug.java
@@ -51,7 +51,7 @@ public class ChessBug extends Application {
         String[][] menuOptions = {
             {"Dash Board"}, // Home
             {"New Game"}, // Games
-            {"Preferences", "About"}  // Setting
+            {"Preferences", "About"},  // Setting
             {"User Profile"} // Profile (added menu option)
         };
         fillMenuBar(menuBar, menus, menuOptions); //Creates dashboard based on above arrays
@@ -91,21 +91,20 @@ public class ChessBug extends Application {
 
         switch (newPage){
             case "New Game" -> openNewGame();
-            default -> page.getChildren().add(new Label("Debug: " + newPage));
-            case "Preferences";
-            //Navigate to prefrences
-            System.out.println("Navigating to Prefrences...");
-            break;
-            case "About":
+            case "Preferences" -> {
+                //Navigate to prefrences
+                System.out.println("Navigating to Prefrences...");}
+            case "About" -> {
                 // Navigate to About page
                 System.out.println("Showing About page...");
-                break;
-            case "User Profile":
+                }
+            case "User Profile" -> {
+                
                 // Navigate to User Profile page
                 System.out.println("Navigating to User Profile...");
-                break;
-            default:
-                System.out.println("Unknown option selected: " + selectedOption);
+            }
+            
+            default -> page.getChildren().add(new Label("Debug: " + newPage));
         }
     }
     
@@ -114,108 +113,6 @@ public class ChessBug extends Application {
         page.getChildren().add(gamePage.getGameBoard());
         
     }
-//    DELETE
-    //private GridPane createGameBoard(boolean isWhitePerspective){
-//        //Start new game
-//        game = new ChessGame(promotionLambda);
-//        
-//        //Create chessBoard
-//        GridPane chessBoard = new GridPane();
-//        chessBoard.getStyleClass().add("chessBoard");
-//        
-//        //Layout chess board within chessBoard =================================
-//        for (int row = 0; row < 8; row++) {
-//            //Create notation labels for the board -----------------------------
-//            //Collumn labels:   a, b, c, d, e, f, g, h
-//            Label colLabel = new Label(String.valueOf((char) ('a' + row))); //Create label
-//            chessBoard.add(colLabel, (isWhitePerspective)? row + 1 : 8 - row, 8); //Add label to chessBoard
-//            GridPane.setHalignment(colLabel, HPos.CENTER); //specify label alignment
-//
-//            //Row labels:       8, 7, 6, 5, 4, 3, 2, 1
-//            Label rowLabel = new Label(String.valueOf(8 - row)); //Create label
-//            chessBoard.add(rowLabel, 0, (isWhitePerspective)? row : 7 - row); //Add label to chessBoard
-//            //specify label alignment
-//            GridPane.setValignment(rowLabel, VPos.CENTER);
-//            GridPane.setHalignment(rowLabel, HPos.RIGHT);
-//            //------------------------------------------------------------------
-//
-//            //Create each board square in the row ------------------------------
-//            for (int col = 0; col < 8; col++) {
-//                //Create new pane to represent square
-//                BorderPane square = new BorderPane();
-//                //Set square ID to notational square name (e.g., b4, c6, h1, etc.)
-//                square.setId("" + (char) ('a' + col) + (row + 1));
-//                
-//                //Give squares a class based on color to create checkered pattern
-//                if ((row + col) % 2 != 0) {
-//                    square.getStyleClass().add("white");
-//                } else {
-//                    square.getStyleClass().add("black");
-//                }
-//
-//                //Square styles
-//                //Specify size for square
-//                int squareSize = 50;
-//                square.setMinHeight(squareSize);
-//                square.setMinWidth(squareSize);
-//                //Align square
-//                GridPane.setValignment(square, VPos.CENTER);
-//                GridPane.setHalignment(square, HPos.CENTER);
-//
-//                //TODO: Add functionality to pressing a square
-//                //square.setOnMouseClicked(event -> boardInteraction(chessBoard, square));
-//
-//                //Add square to chessBoard
-//                chessBoard.add(square, (isWhitePerspective)? col + 1 : 8 - col, (isWhitePerspective)? 7 - row : row);
-//            }
-//        }
-//        //----------------------------------------------------------------------
-//        updateGameDisplay(chessBoard);
-//        
-//        return chessBoard;
-//    }
-    
-//    private void updateGameDisplay(GridPane chessBoard){
-//         //Update each square in chessBoard to reflect game's condition
-//        /*
-//        Note: we only need to modify squares (not labels), and all squares are
-//        created from the BoarderPane class, so we can modify all BoarderPane
-//        children of chessBoard.
-//        */
-//        chessBoard.getChildren().forEach(child -> {
-//            if (child instanceof BorderPane square) {
-//                //System.out.println("Debug: Board id: " + board.getParent().getId() + ", game list size: " + gameList.size());
-//                
-//                //Clear the square's display content
-//                square.getChildren().clear();
-//        
-//                //Get the piece inhabiting the corresponding location in the game
-//                Piece piece = game.getLocalPiece(square.getId());
-//                //If the piece is not null, load the correct image representation of the piece
-//                if (piece != null) {
-//                    //Determine imageFileName for based on the piece
-//                    String imageFileName = (piece.getColor()) ? "white" : "black";
-//                    imageFileName += piece.getClass().getSimpleName();
-//                    
-//                    //Load corresponding image
-//                    try (FileInputStream imageFile = new FileInputStream("pieceImages/" + imageFileName + ".png")) {
-//                        //Create image
-//                        ImageView icon = new ImageView(new Image(imageFile));
-//                        //Style image
-//                        icon.setFitHeight(square.getMinHeight() - 6); //Set Height of image. Note x - 6 allows for insets of 3px
-//                        icon.setPreserveRatio(true); //Maintain ratio
-//                        //Add image
-//                        square.setCenter(icon);
-//                    } catch (IOException e) {
-//                        //If image is not found, use a label to hold the piece's string representation
-//                        square.getChildren().add(new Label(piece.toString()));
-//                    }
-//                }
-//            }
-//        });
-//        //======================================================================
-//        
-//    }
     
     public static void main(String[] args) {
         Application.launch(args);
