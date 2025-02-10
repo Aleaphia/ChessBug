b1fd3ffcca12a8232448ad992c9822633b93bac0
aleaphia <shosh.b.lily@gmail.com>
2025-02-09 20:38:08 -0600

    added friend request UI and removed some old unused files

---
+++ ChessGameApp/src/chessBug/FriendRequestUI.java
@@ -0,0 +1,35 @@
+package chessBug;
+
+import chessBug.network.*;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.json.JSONObject;
+
+
+
+
+public class FriendRequestUI {
+    private VBox page = new VBox();
+    Client client;
+    
+    FriendRequestUI(Client client){
+        this.client = client;
+        
+        //Create UI Layout
+        TextField input = new TextField();
+        Button btnSearch = new Button("Send");
+        page.getChildren().addAll(new Label("Search by Username:"), input, btnSearch);
+        
+        //Add functionality
+        input.setOnAction(event -> sendFriendRequest(input.getText()));
+        btnSearch.setOnAction(event -> sendFriendRequest(input.getText()));
+    }
+    
+    private void sendFriendRequest(String friendUsername){
+        JSONObject out = new JSONObject();
+    }
+    
+}
